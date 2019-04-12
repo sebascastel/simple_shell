c5bd1ea0ab1596e716cd74303594e45d7b901ce8
andres-martin <andresmartin266@gmail.com>
2019-04-12 23:32:29 +0000

    welcome.c fixed

--- welcome.c
+++ welcome.c
@@ -5,30 +5,17 @@
  */
 int welcome(void)
 {
-	int txt_file, total, read_status;
-	size_t letters = 5000;
 	char *filename = "welcome.txt";
-	char buffer[1024];
+	FILE *fptr = NULL;
+	char *error = "error opening file \n";
 
-	if (filename == NULL)
-		return (0);
-	txt_file = open(filename, O_RDONLY);
-
-	if (txt_file == -1)
-		return (0);
-	total = 0;
-	read_status = 1;
-
-	while (letters > 1024 && read_status != 0)
+	fptr = fopen(filename, "r");
+	if (!fptr)
 	{
-		read_status = read(txt_file, buffer, 1024);
-		write(STDOUT_FILENO, buffer, read_status);
-		total += read_status;
-		letters -= 1024;
+		write(STDERR_FILENO, error, _strlen(error));
+		return (1);
 	}
-	read_status = read(txt_file, buffer, letters);
-	write(STDOUT_FILENO, buffer, read_status);
-	total += read_status;
-	close(txt_file);
-return (1);
+	display_splash_screen(fptr);
+	fclose(fptr);
+return (0);
 }
