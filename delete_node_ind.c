491f883818e6aafc6a6462654484adb320da3315
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-05-02 10:05:41 -0500

    Update delete_node_ind.c

--- delete_node_ind.c
+++ delete_node_ind.c
@@ -7,26 +7,26 @@
  */
 int delete_node_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev_node = NULL;
-	listint_t *tgt_node = NULL;
+	listint_t *prev_node = NULL, target_node = NULL;
+
 	unsigned int counter = 0;
 
 	if (!*head)
 		return (-1);
-	tgt_node = *head;
-	while (counter < index && tgt_node)
+	target_node = *head;
+	while (counter < index && target_node)
 	{
-		prev_node = tgt_node;
-		tgt_node = tgt_node->next;
+		prev_node = target_node;
+		target_node = target_node->next;
 		counter++;
 	}
 	if (counter != index)
 		return (-1);
 	if (!prev_node)
-		*head = tgt_node->next;
+		*head = target_node->next;
 	else
-		prev_node->next = tgt_node->next;
-	free(tgt_node->a);
-	free(tgt_node);
+		prev_node->next = target_node->next;
+	free(target_node->a);
+	free(target_node);
 return (1);
 }
