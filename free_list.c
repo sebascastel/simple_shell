60db8d07ee577f63e6eed6e9af5b8307cf661769
sebascastel <47120815+sebascastel@users.noreply.github.com>
2019-05-02 10:00:54 -0500

    Update free_list.c

--- free_list.c
+++ free_list.c
@@ -4,12 +4,21 @@
  * @head: pointer to head of list
  */
 void free_list(listint_t *head)
+
 {
-	if (head)
-	{
-		if (head->next)
-			free_list(head->next);
-		free(head->a);
-		free(head);
-	}
+    listint_t *next;
+    while (head)
+    {
+        next = head->next;
+        free(head);
+        head = next;
+    }
+    return;
 }
+	
+
+
+
+
+	
+
